b366923ca187e213098af5ae2c757ab87de226a3
abdo <abdomansour975@gmail.com>
2023-02-20 12:03:42 -0800

    aad

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - a void fn that prints a sentence
+ * Return: 1
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = strlen(str);
+
+	write(2, str, len);
+	return (1);
+}
